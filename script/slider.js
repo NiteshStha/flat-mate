295c3afb6125420f90ad906ad9f007ec575d1735
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2019-07-12 16:54:05 +0545

    Slider

--- script/slider.js
+++ script/slider.js
@@ -1,9 +1,23 @@
-class ImageSlider {
-  constructor() {
-    this.element = this.posX = 0;
+var sliderImages = document.querySelectorAll('.slider-image img');
+
+var leftBtn = document.getElementById('leftBtn');
+var rightBtn = document.getElementById('rightBtn');
+
+var imageIndex = 0;
+var sliderImageLength = sliderImages.length;
+
+leftBtn.addEventListener('click', function() {
+  if (imageIndex > 0) {
+    sliderImages[imageIndex].setAttribute('class', 'deactive-image');
+    imageIndex--;
+    sliderImages[imageIndex].setAttribute('class', 'active-image');
   }
+});
 
-  moveLeft = () => {};
-
-  moveRight = () => {};
-}
+rightBtn.addEventListener('click', function() {
+  if (imageIndex < sliderImageLength - 1) {
+    sliderImages[imageIndex].setAttribute('class', 'deactive-image');
+    imageIndex++;
+    sliderImages[imageIndex].setAttribute('class', 'active-image');
+  }
+});
--- script/video.js
+++ script/video.js
@@ -1,11 +1,12 @@
-var videoBody = document.getElementById('videoBody');
 var playBtn = document.getElementById('playBtn');
 var videoBg = document.getElementById('videoBg');
 var video = document.getElementById('video');
-console.log(video.src);
 
-playBtn.addEventListener('click', () => {
+playBtn.addEventListener('click', playVideo);
+videoBg.addEventListener('click', playVideo);
+
+function playVideo() {
   playBtn.style.display = 'none';
   videoBg.style.display = 'none';
   video.src = video.src + '?playlist=i4Zg6_yKOh8&autoplay=1';
-});
+}
